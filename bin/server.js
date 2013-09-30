5581e34a6d526d88786c3782a463e38b9d345966
Alex Fernández <alex.fernandez@mediasmart.es>
2013-10-01 01:08:52 +0200

    Added more short options, documented all of them.

--- bin/server.js
+++ bin/server.js
@@ -10,6 +10,19 @@
 var args = require('optimist').argv;
 var server = require('../lib/server');
 
+var optionMap = {
+	dir: 'directory',
+	testdir: 'testDirectory',
+	name: 'packageName',
+	exec: 'deploymentCommand',
+	user: 'emailUser',
+	password: 'emailPassword',
+	host: 'emailHost',
+	ssl: 'emailSsl',
+	from: 'emailFrom',
+	to: 'emailTo',
+};
+
 // init
 if(args.help || args.h)
 {
@@ -26,9 +39,11 @@ if(args._.length > 0)
         help();
     }
 }
-args.directory = args.dir;
-args.testDirectory = args.testdir;
-args.deploymentCommand = args.exec;
+for (var shortOption in optionMap)
+{
+	var longOption = optionMap[shortOption];
+	args[longOption] = args[shortOption];
+}
 server.startServer(args);
 
 /**
@@ -38,12 +53,21 @@ function help()
 {
 	console.log('Usage: deployment-server [options] [port]');
 	console.log('  starts a deployment server on the given port, default 3470.');
-	console.log('Options are:');
-	console.log('    --token [token]   Security token for the URL');
+	console.log('At least one of these options should be enabled:');
 	console.log('    --dir [path]      Directory to deploy to');
 	console.log('    --testdir [path]  Directory with a test environment');
+	console.log('Optional options:');
+	console.log('    --token [token]   Security token for the URL');
+	console.log('    --name [name]     Package name to show in messages');
 	console.log('    --exec [command]  Command to run after success');
 	console.log('    --quiet           Do not show log messages');
+	console.log('Email-related options:');
+	console.log('    --user [user]     User for email server');
+	console.log('    --password [pwd]  Password for email server');
+	console.log('    --host [host]     Host for email server');
+	console.log('    --ssl [boolean]   "true" to enable SSL');
+	console.log('    --from [email]    Email address that generates the message');
+	console.log('    --to [email]      Destination for deployment message');
 	process.exit(0);
 }
 
